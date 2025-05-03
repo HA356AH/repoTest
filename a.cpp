bb6f6b6a62be9faa734bc98561b938f0e7df5ace
Henry Andrew <username@extension.com>
2025-05-03 13:28:54 -0500

    update

--- a.cpp
+++ a.cpp
@@ -1,1 +1,29 @@
-// hola rogger
+// C++ program to implement
+// dice throw problem using recursion
+#include <bits/stdc++.h>
+using namespace std;
+
+int noOfWays(int m, int n, int x) {
+
+    // Base case: Valid combination
+    if (n == 0 && x == 0)
+        return 1;
+
+    // Base case: Invalid combination
+    if (n == 0 || x <= 0)
+        return 0;
+
+    int ans = 0;
+
+    // Check for all values of m.
+    for (int i = 1; i <= m; i++) {
+        ans += noOfWays(m, n - 1, x - i);
+    }
+
+    return ans;
+}
+
+int main() {
+    int m = 6, n = 3, x = 8;
+    cout << noOfWays(m, n, x);
+}
